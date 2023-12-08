728a5bae020843565fc05e09ea65beef54249bb0
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-08 23:21:20 +0100

    landingpage

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from views.Routing import Route
 
 def main(page: Page):
     page.theme_mode = "dark"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    page.padding = 0
 
     myroute = Route(page)
     page.on_route_change = myroute.change_route
     page.add(myroute.body)
     page.go("/")
+    page.update()
 
 
 if __name__ == "__main__":
--- views/landingpage.py
+++ views/landingpage.py
@@ -2,15 +2,38 @@ from flet import *
 
 
 def LandingPage(page):
-    content = Column(
+    content = Stack(
+        # expand=True,
         controls=[
             Container(
-                width=400,
-                height=500,
-                bgcolor="green",
+                # expand=True,
+                height=800,
+                gradient=LinearGradient(
+                    begin=alignment.center_left,
+                    end=alignment.center_right,
+                    colors=["#442063", "#1d3263"],
+                ),
+                # padding=padding.all(0),
                 alignment=alignment.center,
-                content=Text("This the landing page"),
-            )
-        ]
+            ),
+            Text("ATOMIZER", left=10, top=5),
+            Column(
+                # expand=True,
+                controls=[
+                    Container(
+                        expand=True,
+                        width=500,
+                        # height=300,
+                        bgcolor="red",
+                        content=Text(
+                            "Your AI-Powered Science Problem- Solving Companion",
+                            size=50,
+                        ),
+                    ),
+                ],
+                left=20,
+                top=50,
+            ),
+        ],
     )
     return content
