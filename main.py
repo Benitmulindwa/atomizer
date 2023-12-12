c5ed2583d34c02a671db3c85c7ae0fc6ff5b0823
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-12 22:51:08 +0100

    update landingpage

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flet import *
-
 from views.Routing import Route
+from flet import *
 
 
 def main(page: Page):
     page.theme_mode = "dark"
     page.vertical_alignment = "center"
     page.horizontal_alignment = "center"
-    page.padding = 10
+    page.padding = 0
 
     myroute = Route(page)
     page.on_route_change = myroute.change_route
--- views/landingpage.py
+++ views/landingpage.py
@@ -1,4 +1,5 @@
 from flet import *
+from time import sleep
 
 # ---------------------------------------------------------------------------------------------------------------#
 ## gradient Background ##
@@ -29,16 +30,7 @@ from flet import *
 #             ),
 
 # ---------------------------------------------------------------------------------------------------------------#
-landing_text = Container(
-    expand=True,
-    width=500,
-    content=Text(
-        "Your AI-Powered Science Problem - Solving Companion", expand=True, size=60
-    ),
-    alignment=alignment.center,
-)
 
-landing_text.padding = padding.only(left=20, top=30)
 # ---------------------------------------------------------------------------------------------------------------#
 ################### GET STARTED BUTTOM #####################################
 # ---------------------------------------------------------------------------------------------------------------#
@@ -50,44 +42,102 @@ getstarted = Container(
     alignment=alignment.center,
     border_radius=5,
 )
-getstarted.padding = padding.only(left=50)
+getstarted.margin = margin.only(left=20)
+
 # ---------------------------------------------------------------------------------------------------------------#
+#################### LOGIN BUTTON #########################
+# ---------------------------------------------------------------------------------------------------------------#
+login_bt = Container(
+    # expand=True,
+    width=100,
+    height=20,
+    content=Text("LOGIN"),
+    bgcolor="#8919db",
+    alignment=alignment.center,
+    border_radius=50,
+)
+
+# ---------------------------------------------------------------------------------------------------------------#
+#################### Landing Text #########################
+# ---------------------------------------------------------------------------------------------------------------#
+
+landing_text = Container(
+    expand=True,
+    height=500,
+    content=Text(
+        "Your AI-Powered Science Problem - Solving Companion", expand=True, size=60
+    ),
+    alignment=alignment.center,
+)
+
+# landing_text.content.value = ""
+# for word in landing_text.content.value.split(" "):
+#     landing_text.content.value = "".join(word)
+#     landing_text.content.update()
+#     sleep(0.008)
+
+landing_text.padding = padding.only(left=20, top=30)
 
 # ---------------------------------------------------------------------------------------------------------------#
 
 
 def LandingPage(page):
-    page.bgcolor = "#1d3263"
     return Column(
         horizontal_alignment=CrossAxisAlignment.CENTER,
+        auto_scroll=True,
         controls=[
-            Row(
+            Stack(
                 controls=[
-                    Container(height=20, width=20, bgcolor="blue"),
-                    Text("ATOMIZER"),
-                    Row(expand=True),
                     Container(
-                        # expand=True,
-                        content=Text("LOGIN", expand=True),
-                        bgcolor="#8919db",
+                        expand=True,
+                        height=800,
+                        gradient=LinearGradient(
+                            begin=alignment.center_left,
+                            end=alignment.center_right,
+                            colors=["#442063", "#1d3263"],
+                        ),
                         alignment=alignment.center,
-                        border_radius=50,
                     ),
-                ],
-            ),
-            Column(
-                auto_scroll=True,
-                expand=True,
-                controls=[
                     Row(
-                        # expand=True,
                         controls=[
-                            landing_text,
-                            Container(height=500, expand=True, bgcolor="red"),
+                            Container(height=30, width=30, bgcolor="blue"),
+                            Text("ATOMIZER"),
+                            Row(expand=True),
+                            login_bt,
+                        ],
+                        # top=10,
+                    ),
+                    Column(
+                        controls=[
+                            Row(
+                                controls=[
+                                    Row(
+                                        controls=[
+                                            # Container(
+                                            #     expand=True,
+                                            #     width=500,
+                                            #     content=Text(
+                                            #         "Your AI-Powered Science Problem - Solving Companion",
+                                            #         # expand=True,
+                                            #         size=60,
+                                            #     ),
+                                            #     alignment=alignment.center,
+                                            # )
+                                            # landing_text,
+                                            Container(
+                                                expand=True, height=500, bgcolor="blue"
+                                            ),
+                                            Text("gfgfgfgfytrytrhhgjhjuyuy"),
+                                        ],
+                                    ),
+                                    # landing_text,
+                                    Container(height=300, expand=True, bgcolor="red"),
+                                ]
+                            ),
+                            getstarted,
                         ],
                     ),
-                    getstarted,
-                ],
-            ),
+                ]
+            )
         ],
     )
