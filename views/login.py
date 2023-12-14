da7b45622fbacadaaa101711530848a147ed9222
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-14 17:12:41 +0100

    finish the login ui

--- views/login.py
+++ views/login.py
@@ -9,6 +9,7 @@ class MyLoginpage(UserControl):
         self.page = page
 
     def build(self):
+        # print(self.custom_textfield("vagaga").controls[1].content)
         self.login_text = Container(
             content=Text(
                 "CREATE  AN  ACCOUNT",
@@ -27,6 +28,37 @@ class MyLoginpage(UserControl):
                     self.custom_textfield("Username:"),
                     self.custom_textfield("Email:"),
                     self.custom_textfield("Password:", True),
+                    Container(
+                        Text(
+                            value="DO YOU HAVE AN ACCOUNT?",
+                            font_family="lastica",
+                            size=12,
+                            spans=[
+                                TextSpan(
+                                    " Login",
+                                    TextStyle(
+                                        color="#d73cbe",
+                                        font_family="lastica",
+                                        size=12,
+                                        weight=FontWeight.BOLD,
+                                    ),
+                                    on_click=self._go_to_login,
+                                ),
+                            ],
+                        ),
+                    ),
+                    Container(
+                        Text(
+                            "Create an account",
+                            weight=FontWeight.BOLD,
+                            text_align="center",
+                        ),
+                        bgcolor="#8919db",
+                        border_radius=5,
+                        width=150,
+                        margin=margin.only(top=20),
+                        padding=padding.only(bottom=4),
+                    ),
                 ],
                 horizontal_alignment=CrossAxisAlignment.CENTER,
             ),
@@ -34,10 +66,17 @@ class MyLoginpage(UserControl):
             height=500,
             bgcolor="transparent",
             border=border.all(2, "white"),
+            shadow=BoxShadow(
+                spread_radius=1,
+                blur_radius=15,
+                color="#d73cbe",
+                offset=Offset(0, 0),
+                blur_style=ShadowBlurStyle.OUTER,
+            )
             # opacity=0.3,
         )
         self._container.margin = margin.only(top=100)
-        # self._container.padding = padding.only(top=10)
+
         return Container(
             content=Stack(
                 [
@@ -59,16 +98,16 @@ class MyLoginpage(UserControl):
             gradient=LinearGradient(
                 begin=alignment.center_left,
                 end=alignment.center_right,
-                colors=["#1d3263", "#d73cb"],
+                colors=["#1d3263", "#442063"],
             ),
         )
 
     def custom_textfield(self, name, password_state: bool = False):
         return Column(
-            [
+            controls=[
                 Container(
                     Text(name, font_family="lastica"),
-                    padding=padding.only(left=10, right=10, top=15),
+                    padding=padding.only(left=10, right=10, top=15, bottom=0),
                 ),
                 Container(
                     TextField(
@@ -83,10 +122,13 @@ class MyLoginpage(UserControl):
                         password=password_state,
                         can_reveal_password=password_state,
                     ),
-                    padding=padding.only(left=10, right=10, top=5),
+                    padding=padding.only(left=10, right=10, top=0, bottom=15),
                 ),
             ]
         )
+
+    def _go_to_login(self, e):
+        return self.page.go("/login")
 
 
 def Login(page):
