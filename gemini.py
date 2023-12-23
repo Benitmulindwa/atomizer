a964d9a33d34d6dc65b5d1d6f77479176823bd84
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-23 23:15:02 +0100

    update

--- gemini.py
+++ gemini.py
@@ -1,26 +1,44 @@
-# import pathlib
-# import textwrap
+"""
+At the command line, only need to run once to install the package via pip:
+$ pip install google-generativeai
+"""
 
-# import google.generativeai as genai
+import google.generativeai as genai
+import os
 
-# from IPython.display import display
-# from IPython.display import Markdown
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# from dotenv import load_dotenv
+# Set up the model
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
 
-# import os
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+    },
+]
 
-# load_dotenv()
+model = genai.GenerativeModel(
+    model_name="gemini-pro",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
 
-# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+convo = model.start_chat(history=[])
 
-# genai.configure(api_key=GOOGLE_API_KEY)
-
-
-# def to_markdown(text):
-#     text = text.replace("•", "  *")
-#     return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
-
+convo.send_message("WHO ARE YOU?")
+print(convo.last.text)
 
 # model = genai.GenerativeModel("gemini-pro")
 
